2788d38690d2ab08177222d80035f970d714afda
AyoDev <32864849+AyoDev@users.noreply.github.com>
2019-01-15 12:41:18 +0000

    Update lexerX.py

--- Math/lexerX.py
+++ Math/lexerX.py
@@ -6,22 +6,23 @@ tokens = {
     'add': '\+',
     'sub': '\-',
     'equ': '\=',
+    'var': '',
     'end': '\;',
     'fac': '\!',
     'mul': '\*',
+    'var': '"[a-zA-Z][a-zA-Z0-9]*"',
     'open-paren':'\(',
     'close-paren':'\)',
     'open-brace':'\{',
-    'close-brace':'\}',
-    'newline':';',
-    'newline':'\n'
+    'close-brace':'\}'
 }
 
 for i in range(len(tokens)):
     lexer.add([*tokens][i], tokens[[*tokens][i]])
 
 lexer.ignore('\s+')
-
+lexer.ignore('\;')
+lexer.ignore('\n')
 
 l = lexer.build()
 
